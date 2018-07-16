1ddef59a77489ae8ef59c207c1becc854f2c71bf
알렉스 <alexaquino@daum.net>
2018-07-16 13:35:02 +0900

    fix cors for delete request

--- src/main/java/com/aquino/board/config/ResourceConfiguration.java
+++ src/main/java/com/aquino/board/config/ResourceConfiguration.java
@@ -5,11 +5,17 @@
  */
 package com.aquino.board.config;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  *
@@ -19,12 +25,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceConfiguration extends ResourceServerConfigurerAdapter {
 
+    @Value("${frontend.domain}")
+    private String frontendDomain;
+    
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/posts/**")
+        http.antMatcher("/posts/**").cors().and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.DELETE).authenticated()
                 .antMatchers(HttpMethod.POST).authenticated()
                 .anyRequest().permitAll();
     }
+
 }
--- src/main/java/com/aquino/board/config/WebSecurityConfiguration.java
+++ src/main/java/com/aquino/board/config/WebSecurityConfiguration.java
@@ -86,7 +86,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200");
             }
         };
     }
