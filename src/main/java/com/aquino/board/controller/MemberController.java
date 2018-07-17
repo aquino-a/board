e84c8f59ab12f7a5d8ae2c4ae860656856991aa9
알렉스 <alexaquino@daum.net>
2018-07-17 14:36:36 +0900

    fixed logout, complete

--- src/main/java/com/aquino/board/controller/MemberController.java
+++ src/main/java/com/aquino/board/controller/MemberController.java
@@ -8,19 +8,13 @@ package com.aquino.board.controller;
 import com.aquino.board.model.Member;
 import com.aquino.board.repositories.AddressRepository;
 import com.aquino.board.repositories.UserRepository;
-import java.io.IOException;
-import java.security.Principal;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,7 +22,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -76,6 +69,11 @@ public class MemberController {
     public String logOut(
             HttpServletRequest request, HttpServletResponse response,
             Model model, @RequestParam(value="redirect_url", required=true)String redirectUrl) {
+        HttpSession session= request.getSession(false);
+        SecurityContextHolder.clearContext();
+        if(session != null) {
+            session.invalidate();
+        }
         Cookie[] cookies = request.getCookies();
         if(cookies != null) {
             for (Cookie cookie : cookies) {
